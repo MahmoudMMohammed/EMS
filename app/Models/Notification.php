685f05285b7a3e4304b084d8429248ae9125f537
Mahmoud Mohammad <mahmoudmohammad932@gmail.com>
2024-04-20 00:53:42 +0300

    add notification model, migration and controller

---
+++ app/Models/Notification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+    protected $table = 'notifications';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'updated_at',
+    ];
+}
