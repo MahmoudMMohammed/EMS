a967ea4e466414ff48199aff2076a86e018cfd79
Mahmoud Mohammad <mahmoudmohammad932@gmail.com>
2024-04-20 16:13:39 +0300

    registering middlewares and updating api.php file

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SocialController;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\OwnerMiddleware;
+use App\Http\Middleware\UserMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,17 +19,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//register new user
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+
+//verify user email to continue to the app
 Route::post('/verify-email', [AuthController::class, 'verifyEmail']);
 
+//login existing user
+Route::post('/login', [AuthController::class, 'login']);
+
+
+
+//requesting code through email to change password
 Route::post('/password/email',  [AuthController::class , 'forgotPassword']);
+
+//checking code validity
 Route::post('/password/code/check', [AuthController::class , 'checkCode']);
+
+//resetting user password
 Route::post('/password/reset', [AuthController::class , 'resetPassword']);
 
+
+
+//redirecting to google for authentication
 Route::get('/auth/google', [SocialController::class, 'redirectToGoogleAPI']);
+
+//handling google callback with user info
 Route::get('/auth/google/callback' ,[SocialController::class, 'handleGoogleAPICallback']);
+
+
+Route::middleware([UserMiddleware::class])->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+
+});
+
+Route::middleware([AdminMiddleware::class])->group(function () {
+
+});
+
+Route::middleware([OwnerMiddleware::class])->group(function () {
+
+});
 
 //test
 //Route::get('/notification/user/{user_id}' ,[AuthController::class, 'testNotifications']);
