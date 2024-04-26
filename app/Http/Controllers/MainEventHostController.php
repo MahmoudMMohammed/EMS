8e188e20900012d956bcb5c35ec84f94aec2dc02
Mohammad Mansour <twix.shot.2020@gmail.com>
2024-04-26 20:43:40 +0300

    method that give me all hosts related of each event in home screen

--- app/Http/Controllers/MainEventHostController.php
+++ app/Http/Controllers/MainEventHostController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainEventHostController extends Controller
 {
-    //
+    public function GetHostsRelatedEvent($id): JsonResponse
+    {
+        $event = DB::table('main_event_hosts')
+            ->join('hosts', 'main_event_hosts.host_id', '=', 'hosts.id')
+            ->join('main_events', 'main_event_hosts.main_event_id', '=', 'main_events.id')
+            ->where('main_event_id' , $id)
+            ->select('main_events.name as event_name' , 'hosts.name as host_name' , 'hosts.picture' , 'hosts.recommended_for')
+            ->get();
+
+        if(!$event)
+        {
+            return response()->json([
+                'message' => 'Something went wrong , try again later' ,
+                'status' => 400
+                ] , 400);
+        }
+        return response()->json($event , 200);
+    }
 }
