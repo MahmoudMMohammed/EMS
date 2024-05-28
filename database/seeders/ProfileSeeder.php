233c0c22faed396681f1e4781d0662777f9494cc
Mohammad Mansour <twix.shot.2020@gmail.com>
2024-05-28 19:54:25 +0300

    add phone numbers of admin to profile seeder

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -15,6 +15,40 @@ class ProfileSeeder extends Seeder
     {
         $user_id = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 , 21 , 22 , 23 , 24 , 25 , 26 , 27 , 28];
 
+        $phone_number = [
+            '0981692323' ,
+            '0992653253' ,
+            '0997561410' ,
+            '0959196261' ,
+            '0994781267' ,
+            '0997933208' ,
+            '0992817928' ,
+            '0981544758' ,
+            '0994273569' ,
+            '0968275808' ,
+            '0993621541' ,
+            '0997337466' ,
+            '0942211235' ,
+            '0946901218' ,
+            '0935211793' ,
+            '0966432799' ,
+            '0962692922' ,
+            '0934894321' ,
+            '0945500305' ,
+            '0937130372' ,
+            '0981104287' ,
+            '0997842324' ,
+            '0981695444' ,
+            '0945456631' ,
+            '0978564411' ,
+            '0993484732' ,
+            '0936593673' ,
+            '0930937446' ,
+
+        ];
+
+
+
         $profile_picture = [
             'ProfilePictures/Owners&Admins/001.jpg' ,
             'ProfilePictures/Owners&Admins/002.jpg' ,
@@ -50,7 +84,9 @@ class ProfileSeeder extends Seeder
         {
             Profile::query()->create([
                 'user_id' => $user_id[$i] ,
-                'profile_picture' => $profile_picture[$i]
+                'phone_number' => $phone_number[$i] ,
+                'profile_picture' => $profile_picture[$i] ,
+                'gender' => 'Male'
             ]);
         }
     }
