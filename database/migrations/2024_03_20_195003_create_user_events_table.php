7ce788c6cd2c297870a58d775a2caa431ad4f781
Mahmoud Mohammad <mahmoudmohammad932@gmail.com>
2024-07-27 16:15:11 +0300

    Updating UserEvent model and migration verified attribute to become enum.

--- database/migrations/2024_03_20_195003_create_user_events_table.php
+++ database/migrations/2024_03_20_195003_create_user_events_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->time('start_time');
             $table->time('end_time');
             $table->unsignedInteger('num_people_invited');
-            $table->boolean('verified')->default(false);
+            $table->enum('verified', ['Pending', 'Confirmed', 'Rejected', 'Finished'])->default('Pending');
             $table->timestamps();
         });
     }
