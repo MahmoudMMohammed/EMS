ce1a6dc374ebf30cd0c6aa25ea936631da3c89ea
Mahmoud Mohammad <mahmoudmohammad932@gmail.com>
2024-08-16 12:57:31 +0300

    A lot.

---
+++ app/Models/WalletCharge.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WalletCharge extends Model
+{
+    use HasFactory;
+    protected $table = 'wallet_charges';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string, double>
+     */
+
+    protected $fillable = [
+        'user_id',
+        'amount'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class ,'user_id' , 'id');
+    }
+}
